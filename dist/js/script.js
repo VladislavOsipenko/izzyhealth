83177fb764cd1a2307fef5520805cac226eae6d4
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-01-16 21:38:29 +0200

    second commit (adaptive)

--- dist/js/script.js
+++ dist/js/script.js
@@ -34,5 +34,22 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__li'),
+    hamburger = document.querySelector('.header__hamburger');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+    
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        })
+    })
+    
+
 
 });
--- src/js/script.js
+++ src/js/script.js
@@ -34,5 +34,61 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__li'),
+    hamburger = document.querySelector('.header__hamburger');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+    
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        })
+    })
+    
+    const animItems = document.querySelectorAll('._anim-items');
+
+    if(animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart; 
+                }
+
+                if ((scrollY > animItemOffset - animItemPoint) && scrollY <(animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-no-hide')) {
+                        animItem.classList.remove('_active');
+                    }
+                    
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(), 
+                scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+                scrollTop = window.scrollY || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+        setTimeout(() => {
+            animOnScroll();
+        }, 400);
+        
+
+    }
+
 
 });
